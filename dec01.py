7d193298417f666fbecb29d957d85aeb502fd1fd
Eric Dennison <ericd@netdenizen.com>
2020-12-05 22:23:25 -0500

    Updated from Brython Server: 12/5/2020 10:23:24 PM

--- dec01.py
+++ dec01.py
@@ -1,14 +1,22 @@
 # Advent of code 01
-def checkfirst(nlist):
+def checkfirst(sum, qty, nlist):
     a = nlist[0]
     if not len(nlist):
         return 0
-    for n in nlist[1:]:
-        if a+n == 2020:
-            return a*n
-    return checkfirst(nlist[1:])
+    if (qty == 3):
+        for n in nlist[1:]:
+            if prod := checkfirst(sum-a, 2, nlist[1:]):
+                return a*prod
+        return checkfirst(2020, 3, nlist[1:])
+    else:
+        for n in nlist[1:]:
+            if a+n == sum:
+                return a*n
+        return checkfirst(sum, 2, nlist[1:])
 
 with open("dec01.txt") as f:
     nums = [int(x) for x in f.readlines()]
-    print(checkfirst(nums))
+    print("day 1": checkfirst(2020, 2, nums))
+
+    
     
