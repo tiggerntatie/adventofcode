1912c6a5668d30c48ee6701047bdd9c3581e3ef6
Eric Dennison <ericd@netdenizen.com>
2020-12-06 12:08:21 -0500

    Updated from Brython Server: 12/6/2020 12:08:19 PM

--- dec03.py
+++ dec03.py
@@ -1,1 +1,31 @@
 # advent of code day 3
+
+data = ["..##.......",
+"#...#...#..",
+".#....#..#.",
+"..#.#...#.#",
+".#...##..#.",
+"..#.##.....",
+".#.#.#....#",
+".#........#",
+"#.##...#...",
+"#...##....#",
+".#..#...#.#"]
+
+def charat(forest, x, y):
+    w = len(forest[0])
+    return forest[y][x%w]
+    
+xstep = 3
+ystep = 1
+y = 0
+x = 0
+count = 0
+w = len(data[0])
+while y < len(data):
+    if data[y][x%w] == '#':
+        count += 1
+    x += xstep
+    y += ystep
+    
+print(count)
