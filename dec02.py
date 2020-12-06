e1407dd79fe7b54edbba6cb9daa614a185ec4356
Eric Dennison <ericd@netdenizen.com>
2020-12-05 22:56:33 -0500

    Updated from Brython Server: 12/5/2020 10:56:32 PM

--- dec02.py
+++ dec02.py
@@ -3,3 +3,13 @@
 # 1-3 a: abcde
 # 1-3 b: cdefg
 # 2-9 c: ccccccccc
+#
+# How many passwords are valid?
+import re
+
+count = 0
+prog = re.compile("(\d+)-(\d+) (\S): (\S+)")
+with file("dec02.txt") as f:
+    pwds = f.readlines()
+    for p in pwds:
+        res = prog.search(p)
