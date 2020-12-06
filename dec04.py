06c4544dc873ada3994dbc2fd38370dc6d4dad56
Eric Dennison <ericd@netdenizen.com>
2020-12-06 13:48:11 -0500

    Updated from Brython Server: 12/6/2020 1:48:09 PM

--- dec04.py
+++ dec04.py
@@ -55,6 +55,21 @@ data = [
     "iyr:2011 ecl:brn hgt:59in"
 ]
 
+data = [
+"pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980",
+"hcl:#623a2f",
+"",
+"eyr:2029 ecl:blu cid:129 byr:1989",
+"iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm",
+"",
+"hcl:#888785",
+"hgt:164cm byr:2001 iyr:2015 cid:88",
+"pid:545766238 ecl:hzl",
+"eyr:2022",
+"",
+"iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719"
+]
+
 def validheight(s):
     f = hgtval.search(s)
     if f:
@@ -85,8 +100,8 @@ def finishrecord(d):
 
     
 
-with open("dec04.txt") as f:
-    data = f.readlines()
+#with open("dec04.txt") as f:
+#    data = f.readlines()
 
 passdict = {}
 count = 0
