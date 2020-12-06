3f31626ebe02435514f2fbe14023b5df5edf28c0
Eric Dennison <ericd@netdenizen.com>
2020-12-06 15:19:29 -0500

    Updated from Brython Server: 12/6/2020 3:19:28 PM

--- dec06.py
+++ dec06.py
@@ -1,1 +1,31 @@
 # advent of code day 6
+
+data = [
+    "abc",
+    "",
+    "a",
+    "b",
+    "c",
+    "",
+    "ab",
+    "ac",
+    "",
+    "a",
+    "a",
+    "a",
+    "a",
+    "",
+    "b"
+]
+
+group = []
+groups = []
+for person in data:
+    if person:
+        group.append(person)
+    else:
+        groups.append(group)
+        group = []
+groups.append(group)
+
+print(groups)
