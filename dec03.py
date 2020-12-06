d7c346431a88325595d20fab3f3d53c57e27b145
Eric Dennison <ericd@netdenizen.com>
2020-12-06 12:11:46 -0500

    Updated from Brython Server: 12/6/2020 12:11:45 PM

--- dec03.py
+++ dec03.py
@@ -12,19 +12,28 @@ data = ["..##.......",
 "#...##....#",
 ".#..#...#.#"]
 
+"""
+Right 1, down 1.
+Right 3, down 1. (This is the slope you already checked.)
+Right 5, down 1.
+Right 7, down 1.
+Right 1, down 2.
+"""
+
+slopes = ((1,1),(3,1),(5,1),(7,1),(1,2))
+
 with open("dec03.txt") as f:
     data = f.readlines()
     
-xstep = 3
-ystep = 1
-y = 0
-x = 0
-count = 0
 w = len(data[0])
-while y < len(data):
-    if data[y][x%w] == '#':
-        count += 1
-    x += xstep
-    y += ystep
+for xstep, ystep in slopes:
+    y = 0
+    x = 0
+    count = 0
+    while y < len(data):
+        if data[y][x%w] == '#':
+            count += 1
+        x += xstep
+        y += ystep
     
 print(count)
