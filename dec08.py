ee0db1af6a660fb4b1f332fc364d3397623f903c
Eric Dennison <ericd@netdenizen.com>
2020-12-08 18:29:16 -0500

    Updated from Brython Server: 12/8/2020 6:29:15 PM

--- dec08.py
+++ dec08.py
@@ -1,24 +1,40 @@
 # advent of code day 8
 
-with open("dec08.txt") as f:
+with open("dec08a.txt") as f:
     data = [x.split() for x in f.readlines()]
     
 data = [(x[0],int(x[1])) for x in data]
-print(data)
+#print(data)
 
-pc = 0
+length = len(data)
 acc = 0
-visited = set()
+def isloop():
+    pc = 0
+    global acc = 0
+    visited = set()
+    while not pc in visited:
+        visited.add(pc)
+        instr = data[pc]
+        if instr[0] == "acc":
+            acc += instr[1]
+            pc += 1
+        elif instr[0] == "jmp":
+            pc += instr[1]
+        else:
+            pc += 1
+        if pc == length:
+            return False
+    return True
 
-while not pc in visited:
-    visited.add(pc)
-    instr = data[pc]
-    if instr[0] == "acc":
-        acc += instr[1]
-        pc += 1
-    elif instr[0] == "jmp":
-        pc += instr[1]
+for n, line in enumerate(data):
+    old = line[0]
+    if old == 'jmp':
+        data[n][1] = 'nop'
+    elif old == 'nop':
+        data[n][1] = 'jmp'
+    if isloop():
+        data[n][1] = old
     else:
-        pc += 1
+        break
 
 print(acc)
