c5f6465aaa1526c84d8f7817874fc5b5caaebb08
Eric Dennison <ericd@netdenizen.com>
2020-12-08 18:19:51 -0500

    Updated from Brython Server: 12/8/2020 6:19:51 PM

--- dec08.py
+++ dec08.py
@@ -1,23 +1,24 @@
 # advent of code day 8
 
-with open("dec08a.txt") as f:
+with open("dec08.txt") as f:
     data = [x.split() for x in f.readlines()]
     
 data = [(x[0],int(x[1])) for x in data]
 print(data)
 
-pc = -1
+pc = 0
 acc = 0
 visited = set()
 
 while not pc in visited:
-    pc += 1
-    print(pc)
     visited.add(pc)
     instr = data[pc]
     if instr[0] == "acc":
         acc += instr[1]
+        pc += 1
     elif instr[0] == "jmp":
         pc += instr[1]
+    else:
+        pc += 1
 
 print(acc)
