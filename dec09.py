26bc8232579ced399dee2e107c3efb767aa2bf3d
Eric Dennison <ericd@netdenizen.com>
2020-12-09 13:26:30 -0500

    Updated from Brython Server: 12/9/2020 1:26:30 PM

--- dec09.py
+++ dec09.py
@@ -1,1 +1,12 @@
 # advent of code day 9
+
+source = "dec09a.txt"
+if source == "dec09a.txt":
+    preamble = 5
+else:
+    preamble = 25
+    
+with open(source) as f:
+    data = [int()]
+    
+print(data)
