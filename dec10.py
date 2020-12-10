52df7469694eabb66194379616516786532ba22c
Eric Dennison <ericd@netdenizen.com>
2020-12-10 17:07:52 -0500

    Updated from Brython Server: 12/10/2020 5:07:53 PM

--- dec10.py
+++ dec10.py
@@ -1,7 +1,17 @@
 # advent of code day 10
 
 with open("dec10a.txt") as f:
-    data = sorted([int(x) for x in f.readlines()])
-    
-print(data)
-    
+    data = [0] + sorted([int(x) for x in f.readlines()])
+
+last = 0
+ones = 0
+threes = 0
+for i,j in enumerate(data[1:]):
+    d = j-data[i-1]
+    if d == 1:
+        ones += 1
+    elif d == 3:
+        threes += 1
+threes += 1
+
+print(ones*threes)
