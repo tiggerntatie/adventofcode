2c91aab1ad67d89bb2e674ac8a9a11c8efe3bcf4
Eric Dennison <ericd@netdenizen.com>
2020-12-10 17:11:09 -0500

    Updated from Brython Server: 12/10/2020 5:11:08 PM

--- dec10.py
+++ dec10.py
@@ -3,11 +3,14 @@
 with open("dec10a.txt") as f:
     data = [0] + sorted([int(x) for x in f.readlines()])
 
+print(data)
+
 last = 0
 ones = 0
 threes = 0
 for i,j in enumerate(data[1:]):
-    d = j-data[i-1]
+    d = j-data[i]
+    print(d)
     if d == 1:
         ones += 1
     elif d == 3:
