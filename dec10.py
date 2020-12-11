afac461323bca62322803eb2a729e259be200302
Eric Dennison <ericd@netdenizen.com>
2020-12-10 21:02:13 -0500

    Updated from Brython Server: 12/10/2020 9:02:13 PM

--- dec10.py
+++ dec10.py
@@ -1,6 +1,6 @@
 # advent of code day 10
 
-with open("dec10a.txt") as f:
+with open("dec10b.txt") as f:
     data = [0] + sorted([int(x) for x in f.readlines()])
 
 print(data)
@@ -37,6 +37,6 @@ def numpaths(n):
     
 print(numpaths(0))
 
-print(nodemap)
+#print(nodemap)
     
     
