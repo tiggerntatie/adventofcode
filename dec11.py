133d9828f14f203eef815c910be10c82e987da43
Eric Dennison <ericd@netdenizen.com>
2020-12-11 16:54:57 -0500

    Updated from Brython Server: 12/11/2020 4:54:57 PM

--- dec11.py
+++ dec11.py
@@ -1,1 +1,14 @@
 # advent of code 11
+
+with open("dec11a.txt") as f:
+    data = f.readlines()
+
+w = len(data[0])
+h = len(data)
+persons = [[0]*w]*h
+persons[1][1]=1
+print(persons)
+
+def personat(x,y):
+    pass
+    
