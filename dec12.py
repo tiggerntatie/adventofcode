56efa64b0342abf12d625a5508926916348109ba
Eric Dennison <ericd@netdenizen.com>
2020-12-12 18:24:26 -0500

    Updated from Brython Server: 12/12/2020 6:24:18 PM

--- dec12.py
+++ dec12.py
@@ -1,27 +1,31 @@
 # advent of code day 12
 
-with open("dec12.txt") as f:
+with open("dec12a.txt") as f:
     data = [s.strip() for s in f.readlines()]
 
 sin = {0:0, 90:1, 180:0, 270:-1}
 cos = {0:1, 90:0, 180:-1, 270:0}
     
 x = y = dir = 0
+wx = 10
+wy = 1
 for op, val in [(x[:1],int(x[1:])) for x in data]:
     if op == 'N':
-        y += val
+        wy += val
     elif op == 'S':
-        y -= val
+        wy -= val
     elif op == 'E':
-        x += val
+        wx += val
     elif op == 'W':
-        x -= val
+        wx -= val
     elif op == 'R':
-        dir -= val
+        wx = wx*cos[-val%360] - wy*sin[-val%360]
+        wy = wx*sin[-val%360] + wy*cos[-val%360]
     elif op == 'L':
-        dir += val
+        wx = wx*cos[val%360] - wy*sin[val%360]
+        wy = wx*sin[val%360] + wy*cos[val%360]
     else:
-        x += val*cos[dir%360]
-        y += val*sin[dir%360]
+        x += val*wx
+        y += val*wy
 
 print(abs(x)+abs(y))
