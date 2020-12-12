6741fe220ad0caa05517401ea0d35305fbcbc252
Eric Dennison <ericd@netdenizen.com>
2020-12-12 13:36:04 -0500

    Updated from Brython Server: 12/12/2020 1:35:55 PM

--- dec11.py
+++ dec11.py
@@ -26,60 +26,71 @@ def personnear(x,y):
     if y < h-1:
         tots += persons[y+1][x]
     return tots
-    
-def personfar(x,y):
-    tots = 0
+
+chairmap = {}
+# get list of coordinate tuples of nearby chairs
+def nearchairs(x,y):
+    nears = []
     #upleft
     dist = min(x,y)
     for a in range(1,dist+1):
-        if persons[y-a][x-a]:
-            tots += 1
+        if data[y-a][x-a]=='L':
+            nears.append((x-a,y-a))
             break
     #up
     dist = y
-    for a in range(1,dist+1):
-        if persons[y-a][x]:
-            tots += 1
+    for a in range(1,dist+1)=='L':
+        if data[y-a][x]:
+            nears.append((x,y-a))
             break
     #upright
     dist = min(w-x-1,y)
-    for a in range(1,dist+1):
-        if persons[y-a][x+a]:
-            tots += 1
+    for a in range(1,dist+1)=='L':
+        if data[y-a][x+a]:
+            nears.append((x+a,y-a))
             break
     #right
     dist = w-x-1
-    for a in range(1,dist+1):
-        if persons[y][x+a]:
-            tots += 1
+    for a in range(1,dist+1)=='L':
+        if data[y][x+a]:
+            nears.append((x+a,y))
             break
     #downright
     dist = min(w-x-1,h-y-1)
-    for a in range(1,dist+1):
-        if persons[y+a][x+a]:
-            tots += 1
+    for a in range(1,dist+1)=='L':
+        if data[y+a][x+a]:
+            nears.append((x+a,y+a))
             break
     #up
     dist = h-y-1
-    for a in range(1,dist+1):
-        if persons[y+a][x]:
-            tots += 1
+    for a in range(1,dist+1)=='L':
+        if data[y+a][x]:
+            nears.append((x,y+a))
             break
     #downleft
     dist = min(x,h-y-1)
-    for a in range(1,dist+1):
-        if persons[y+a][x-a]:
-            tots += 1
+    for a in range(1,dist+1)=='L':
+        if data[y+a][x-a]:
+            nears.append((x-a,y+a))
             break
     #left
     dist = x
-    for a in range(1,dist+1):
-        if persons[y][x-a]:
-            tots += 1
+    for a in range(1,dist+1)=='L':
+        if data[y][x-a]:
+            nears.append((x-a,y))
             break
-    return tots
+    chairmap[(x,y)]=nears
+    
+    
+
 
 neighblimit = 5  # 5 for part 2, 4 for part 1
+# map out the chairs
+for x in range(w):
+    for y in range(h):
+        if data[y][x] == 'L':
+            nearchairs(x,y)
+
 while True:
     print(persons)
     newpersons = [[0 for x in range(w)] for y in range(h)]
@@ -93,7 +104,7 @@ while True:
                 if neighblimit == 4:
                     neighbs = personnear(x,y)
                 else:
-                    neighbs = personfar(x,y)
+                    neighbs = sum([persons[c[1]][c[0]] for c in chairmap[(x,y)]])
                 if neighbs == 0:
                     newpersons[y][x] = 1
                 if neighbs >= neighblimit:
