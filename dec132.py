b7f1e3458acef9bf562f15a4651c300560041c70
Eric Dennison <ericd@netdenizen.com>
2020-12-13 16:46:47 -0500

    Updated from Brython Server: 12/13/2020 4:46:47 PM

--- dec132.py
+++ dec132.py
@@ -1,6 +1,6 @@
 # advent of code day 13 part 2
 
-with open("dec13a.txt") as f:
+with open("dec13.txt") as f:
     data = f.readlines()
 available = list(map(lambda x: x if x=='x' else int(x), data[1].strip().split(',')))
 idmap = {key:val for val, key in filter(lambda x: x[1]!='x', enumerate(available))}
