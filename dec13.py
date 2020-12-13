ba8ef8c37baab60f49d6a81c8c7c71a2e5750629
Eric Dennison <ericd@netdenizen.com>
2020-12-13 09:11:40 -0500

    Updated from Brython Server: 12/13/2020 9:11:40 AM

--- dec13.py
+++ dec13.py
@@ -1,3 +1,10 @@
 # advent of code day 13
 
 with open("dec13a.txt") as f:
+    data = f.readlines()
+
+mytime = int(data[0])
+available = filter(lambda x: x!='x', data[1].strip().split(','))
+
+print(mytime)
+print(available)
