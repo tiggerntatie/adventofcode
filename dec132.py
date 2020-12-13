aea4ecfdccaca4e10eab52ac6d72c285ab266eb5
Eric Dennison <ericd@netdenizen.com>
2020-12-13 16:20:22 -0500

    Updated from Brython Server: 12/13/2020 4:20:21 PM

--- dec132.py
+++ dec132.py
@@ -13,9 +13,10 @@ idlist = [id for id in idmap]
 print(idlist)
 for n, id in enumerate(idlist[:-1]):
     delta = idmap[idlist[n+1]]-idmap[idlist[n]]
+    print(delta)
     for i in range(0,id*idlist[n+1],id):
         print(i)
-        if i%idlist[n+1] == delta:
+        if not (i+delta)%idlist[n+1]:
             print('**',i)
     
 
