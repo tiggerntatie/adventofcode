110d9d9f2333b84b68bd5d26f0fd14d527571df2
Eric Dennison <ericd@netdenizen.com>
2020-12-14 18:04:13 -0500

    Updated from Brython Server: 12/14/2020 6:04:10 PM

--- dec14.py
+++ dec14.py
@@ -7,14 +7,11 @@ currmask = 0
 currsetmask = 0
 multipliers = 0
 mem = {}
-maskcount = 0
 
 with open("dec14.txt") as f:
     for cmd in f.readlines():
         if cmd[1] == 'a':
             maskcount += 1
-            if maskcount == 10:
-                break
             # mask command
             mask = cmd[7:]
             masksetlen = mask.count('X')
