931f560720d35cb18e5a71da9ab5a315e1b5e72f
Eric Dennison <ericd@netdenizen.com>
2020-12-14 17:03:02 -0500

    Updated from Brython Server: 12/14/2020 5:02:59 PM

--- dec14.py
+++ dec14.py
@@ -1,1 +1,17 @@
 # advent of code day 14
+
+import re
+memread = re.compile("[=\[(d+)\] = (d+)")
+
+currmask = 0
+mem = {}
+
+with open("dec14a.txt") as f:
+    for cmd in f.readlines():
+        if cmd[1] == 'a':
+            # mask command
+            mask = cmd[7:]
+        else:
+            # mem command
+            res = re.search(cmd)
+            print(dir(res))
